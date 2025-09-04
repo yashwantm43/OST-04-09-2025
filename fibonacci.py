3ad52c53a5508b4211d2b6e2a1f797b75896674b
yashwantm43 <yashwant.m.btech2023@sitpune.edu.in>
2025-09-04 11:23:30 +0530

    Update fibonacci.py
    added comments to the code

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,11 @@
-def fibonacci_series(n):
+#adding comments to code 
+def fibonacci_series(n): # defining function
     fib_series = [0, 1]
-    for i in range(2, n):
+    for i in range(2, n): #for loop
         fib_series.append(fib_series[i-1] + fib_series[i-2])
     return fib_series
 
-def is_palindrome(number):
+def is_palindrome(number): #palindrome function
     return str(number) == str(number)[::-1]
 
 # User input
@@ -13,7 +14,7 @@ fib_series = fibonacci_series(num_terms)
 print("Fibonacci Series:", fib_series)
 
 num_to_check = int(input("Enter a number to check if it is a palindrome: "))
-if is_palindrome(num_to_check):
+if is_palindrome(num_to_check): #if loop
     print(f"{num_to_check} is a palindrome number.")
 else:
     print(f"{num_to_check} is not a palindrome number.")
