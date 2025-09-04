1622e628eaa8a52238961a7ed6743e5824df1cc2
Mansi-082 <mansi.kulkarni.btech2023@sitpune.edu.in>
2025-09-04 11:12:53 +0530

    Create arithmetic_operators.cpp

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+    cout<<"Addition ("<<a<<"+"<<b<<"): "<<(a+b)<<endl;
+    cout<<"Subtraction ("<<a<<"-"<<b<<"): "<<(a-b)<<endl;
+    cout<<"Multiplication ("<<a<<"*"<<b<<"): "<<(a*b)<<endl;
+    cout<<"Division ("<<a<<"/"<<b<<"): "<<(a/b)<<endl;
+    cout<<"Modulas ("<<a<<"%"<<b<<"): "<<(a%b)<<endl;
+
+    return 0;
+}
